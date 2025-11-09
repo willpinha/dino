ed35e857d3f81843d32137fe426961ab73130411
Willian Pinheiro <willpinhaaa@gmail.com>
2025-11-09 11:38:29 -0300

    Update documentation in doc.go for clarity and completeness of HTTP Box features

--- doc.go
+++ doc.go
@@ -5,13 +5,15 @@ HTTP Box is a lightweight library that contains utilities compatible with the ne
 If you want to work directly with net/http, without relying on abstractions, but don't want to
 implement common functionalities from scratch, HTTP Box is the right library for you
 
-HTTP Box provides features that are not in the standard library, such as:
+HTTP Box provides the following features:
 
 - Handlers and middlewares that return errors
 - Centralized error handling
 - Middlewares for various use cases (logging, rate limit, ...)
 - Request utilities:
-	- Automatic validation of body, query and URL parameters
+	- Body validation
+	- Automatic validation of query and URL parameters
 - Response utilities:
+	- Responses for various content types (JSON, XML, ...)
 	- Standard error responses
 */
