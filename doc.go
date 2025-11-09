f18e108b30f5b4250ea29c277ca0ee98c6d11d6d
Willian Pinheiro <willpinhaaa@gmail.com>
2025-11-09 12:06:50 -0300

    Refactor README and doc.go to improve clarity by consolidating the About and Philosophy sections

--- doc.go
+++ doc.go
@@ -1,17 +1,4 @@
 package httpbox
 
 /*
-# About
-
-HTTP Box is a lightweight library that contains utilities compatible with the net/http package.
-If you want to work directly with net/http, without relying on abstractions, but don't want to
-implement common functionalities from scratch, HTTP Box is the right library for you
-
-# Philosophy
-
-HTTP Box doesn't try to be the magic solution that solves all your problems. Instead, it provides a
-thin layer built on top of net/http for functionalities commonly needed when building applications
-
-It also follows the Go philosophy, which is to maintain simplicity and not apply breaking changes or
-major versions all the time. That's why HTTP Box is still in version 1 (just like Go)
-*/
+ */
