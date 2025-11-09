54491b4b23a0b024bb00925e8832a5a9a8bafcc6
Willian Pinheiro <willpinhaaa@gmail.com>
2025-11-09 12:28:13 -0300

    Update documentation in doc.go to clarify usage of HTTP Box with net/http

--- doc.go
+++ doc.go
@@ -1,4 +1,6 @@
 package httpbox
 
 /*
- */
+This documentation teaches you how to use HTTP Box to build applications with net/http. If you want
+to learn more about the project, its philosophy and features, please read the [README]()
+*/
