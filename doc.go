7ef7c5347c248d62858fda6b20e84995b6d7630e
Willian Pinheiro <willpinhaaa@gmail.com>
2025-11-10 09:29:45 -0300

    Enhance documentation in doc.go with detailed explanations of handlers, error handling, and middleware usage

--- doc.go
+++ doc.go
@@ -1,6 +1,89 @@
 package httpbox
 
 /*
-This documentation teaches you how to use HTTP Box to build applications with net/http. If you want
-to learn more about the project, its philosophy and features, please read the [README]()
+# Handlers
+
+Similar to the `net/http` handlers, the [Handler] type represents the core mechanism for processing
+HTTP requests and generating responses. The difference is that in httpbox, handlers return errors
+
+We can define a handler in the following way:
+
+	func MyHandler() httpbox.Handler {
+		return func(w http.ResponseWriter, r *http.Request) error {
+			// Handler logic here
+			return nil
+		}
+	}
+
+These handlers implement the http.Handler interface, and therefore can be used anywhere that interface
+is used. An example of this is in http.ServeMux:
+
+	mux := http.NewServeMux()
+
+	mux.Handle("GET /path", MyHandler())
+
+In fact, http.ServeMux is the ideal multiplexer (router) to use with httpbox. This is because many of
+httpbox's features are built upon the functionalities that http.ServeMux provides
+
+# Error handling
+
+httpbox has a centralized error handling logic. Errors returned by the handlers are automatically
+processed, which greatly simplifies error handling
+
+There are two possibilities for returning an error in a handler: an error of type [Error], or any
+other error that is not of that type
+
+	func KnownErrorHandler() httpbox.Handler {
+		return func(w http.ResponseWriter, r *http.Request) error {
+			return httpbox.NewError(http.StatusBadRequest, "some message")
+		}
+	}
+
+	func UnknownErrorHandler() httpbox.Handler {
+		return func(w http.ResponseWriter, r *http.Request) error {
+			return errors.New("an unknown error occurred")
+		}
+	}
+
+When we return an error of type [Error], it is automatically serialized to JSON in the response body.
+This response contains the following fields:
+
+- `code` (required): The status code of the response
+- `message` (required): A descriptive message about the error
+- `details` (optional): Additional information about the error (any type)
+
+When we return an error that is not of type [Error], httpbox generates a generic internal server
+error response with status code 500. This prevents unhandled errors from being sent in the response,
+which could lead to leaks of sensitive data (e.g. database info). This means that ideally all errors
+returned by a handler are of type [Error]
+
+We use [NewError] to create new errors of type [Error]. In addition to passing the status code and
+message, we can also pass the following options:
+
+	func MyHandler() httpbox.Handler {
+		return func(w http.ResponseWriter, r *http.Request) error {
+			return httpbox.NewError(
+				http.StatusNotFound,
+				"resource not found",
+				httpbox.WithDetails("Some additional details"),
+				httpbox.WithInternalError(errors.New("database: record not found")
+				httpbox.WithLog(),
+			)
+		}
+	}
+
+- [WithDetails]: Additional details. Ideal for when the message is insufficient to describe the error.
+	It can be of any type that is serializable to JSON
+- [WithInternalError]: An internal error that caused this error to be returned. This error is not
+	serialized in the response, but can be used for logging or debugging purposes
+- [WithLog]: Indicates that this error should be logged. By default, errors are not logged. It must
+	be used in conjunction with the [AccessLogMiddleware] middleware
+
+# Middlewares
+
+# Request utilities
+
+# Response utilities
+
+# Logging
 */
