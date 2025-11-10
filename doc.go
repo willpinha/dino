c3879f6e90f7378b91539f2403ce8f581c2de676
Willian Pinheiro <willpinhaaa@gmail.com>
2025-11-10 09:55:06 -0300

    Clarify wording in WithInternalError documentation for improved understanding

--- doc.go
+++ doc.go
@@ -75,8 +75,8 @@ message, we can also pass the following options:
 
 - [WithDetails]: Additional details. Ideal for when the message is insufficient to describe the error.
 	It can be of any type that is serializable to JSON
-- [WithInternalError]: An internal error that caused the error to be returned. This error is not
-	serialized in the response, but can be used for logging or debugging purposes
+- [WithInternalError]: An internal error that caused the error to be returned. This internal error is
+	not serialized in the response, but can be used for logging or debugging purposes
 - [WithLog]: Indicates that this error should be logged. By default, errors are not logged. It must
 	be used in conjunction with the [AccessLogMiddleware] middleware
 
